34e312937bd81bf84fc798e7b053617e764c842c
chrisyuaners <chrisyuaners@gmail.com>
2020-09-09 14:23:55 -0400

    queue done

---
+++ queue.js
@@ -0,0 +1,48 @@
+// define Node class with value and next props
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// define Queue class with first, last, and size props
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    // add a Node to the end of the Queue
+    enqueue(val) {
+        const newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return this.size += 1;
+    }
+
+    // remove Node from the beginning of the Queue
+    dequeue() {
+        if (!this.first) return null;
+        const temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size -= 1;
+        return temp.value;
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.dequeue();
+console.log(queue);
