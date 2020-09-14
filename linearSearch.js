4e62cc33762a770a47c1ed5fc6fbb284b59ec12d
chrisyuaners <chrisyuaners@gmail.com>
2020-09-14 15:35:42 -0400

    linear search done

---
+++ linearSearch.js
@@ -0,0 +1,7 @@
+// returns index of given value in an array of ints - O(N)
+function linearSearch(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) return i;
+    }
+    return -1;
+}
